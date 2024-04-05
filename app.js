879b34c5cec9c21655ade1c5fac6dca32400d137
ParixJ <jagadaparik@gmail.com>
2024-04-05 22:50:40 +0530

    Added Documentation In Javascript File

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+//Select Elements For Logic From Html File
 let buttons = document.querySelectorAll("#turn");
 let para = document.querySelector(".winner");
 let reset = document.querySelector(".reset");
 let score1 = document.querySelector("#scorep");
 let score2 = document.querySelector("#scorec");
+//Array With All Elements Of The Game 
 let ele = ["Stone","Paper","Scissor"];
+//Variables For Tracking Player And Computer Score
 let comp_score = 0;
 let player_score = 0;
 
+score1.textContent = `Player Score : ${player_score}`;
+score2.textContent = `Computer Score : ${comp_score}`;
+
+//Add Event Listener To Element For Resetting The Game 
 reset.addEventListener("click",() => {
+    //Emptying Paragraph And Changeing Score Back To 0
     para.textContent = "";
     player_score = 0;
     comp_score = 0;
@@ -15,18 +23,19 @@ reset.addEventListener("click",() => {
     score2.textContent = `Computer Score : ${comp_score}`;
 })
 
-score1.textContent = `Player Score : ${player_score}`;
-score2.textContent = `Computer Score : ${comp_score}`;
-
+//Adding Loop For Selecting All Buttons(Turns) One By One
 buttons.forEach((btn) => {
 
+    //Added Event Listener For Each Button
     btn.addEventListener("click",() => {
 
-
+        //Define Function To Get Winner, Update Score And Display Conclusion
          function winner() {
             
+            //Variable For Gettin A Random Number From 0 to 2 For Computer To Select A Random Turn From Ele Array
             let comp_turn = ele[Math.floor(Math.random() * 3)];
 
+            //Statement To Check Every Possiblity Of Both Winning, Losing And Draw(If Both Choices Are Same)
                 if(btn.textContent == ele[0] && comp_turn == ele[1]) {
                     para.innerText = `Computer Won The Match! ${comp_turn} beats ${btn.textContent}.`; 
                     comp_score += 1;
@@ -62,6 +71,8 @@ buttons.forEach((btn) => {
                 }
             }
          winner();
+
+         //Update And Display Both Player And Computer Scores
          score1.textContent = `Player Score : ${player_score}`;
          score2.textContent = `Computer Score : ${comp_score}`;
     })
